6266874897a57ee6b19c89d56348f20f7ba62bf7
shawinke <stijn.hawinkel@ugent.be>
2017-09-06 12:47:21 +0200

    Implementing matrix-wise response function estimation with immediate restrictions

--- R/F_respFunJacMat.R
+++ R/F_respFunJacMat.R
@@ -1,24 +1,30 @@
-#' Calculates the Jacobian of the score functions of the paramaters of parametric response functions  (a polynomial of any degree)
+#' Calculates the Jacobian of the score functions of the paramaters of parametric response functions  (a polynomial of any degree). Parameters are sorted per taxon, than with increasing degree.
 #'
-#' @param beta: a (deg+1)*p matrix of regression parameters with deg the degree of the response function
-#' @param X: the nxp data matrix
-#' @param reg: a vector of regressors with the dimension (deg+1)*n (\mathbf{C} \boldsymbol{\alpha})
-#' @param theta: The dispersion parameter
-#' @param mumarg: offset matrix of size nxp
-#' @param cReg: a vector of lentgh p psi_k * s_{jk}
+#' @param betas: a vector of length (deg+1)*(p+1) with regression parameters with deg the degree of the response function and the lagrangian multipliers
+#' @param aX the nxp data matrix
+#' @param reg a vector of regressors with the dimension n-by-v (\mathbf{C} \boldsymbol{\alpha})
+#' @param thetaMat The n-by-p matrix with dispersion parameters
+#' @param mumarg offset matrix of size nxp
+#' @param psi a scalar, the importance parameter
+#' @param v an integer, one plus the degree of the response function
+#' @param p an integer, the number of taxa
 #'
 #' @return The jacobian, a square matrix of dimension (deg+1)*(p+1)
 
-respFunJacMat = function(betas, aX, reg, theta, muMarg, cReg) {
-  mu = exp(crossprod(reg, betas)*psi) * muMarg
-  Jac = matrix(0, p*v, p*v)
+respFunJacMat = function(betas, aX, reg, thetaMat, muMarg, lambda2, psi, v, p) {
+  NBparams = matrix(betas[seq_len(p*v)], ncol = p)
+  mu = exp(reg %*% NBparams*psi) * muMarg
+  Jac = matrix(0, (p+1)*v, (p+1)*v)
+  did  = seq_len(p*v)
+  didv = seq_len(v)
+  indVec = matrix(rep(c(TRUE, rep(FALSE, v), TRUE),p), nrow = p*v, byrow = TRUE)
+  Jac[did, didv][indVec] = NBparams
+  Jac[lower.tri(Jac)] = Jac[upper.tri(Jac)]
 
-  v=ncol(reg);p=ncol(aX)
-  tmp = t(t((1+t(t(X)/theta))*mu/(1+t(t(mu)/theta))^2)*cReg^2)
-  tmp2 =  vapply(seq_len(v), FUN.VALUE = tmp, function(x){reg[,x]*tmp})
-  tmp3 =  vapply(seq_len(v), FUN.VALUE = betas, function(x){crossprod(reg,tmp2[,,x])})
+  tmp = (1+X/thetaMat)*mu/(1+(mu/thetaMat)^2)*psi^2
+  tmp2 =  vapply(didv, FUN.VALUE = tmp, function(x){reg[,x]*tmp})
+  Jac[did, did] = tensor(reg, tmp2, 1, 1)
 
-  ind = as.logical(Reduce(f=adiag,lapply(seq_len(p),function(x){matrix(TRUE,v,v)})))
-  Jac[ind] = apply(tmp3,2,identity)
+  diag(Jac)[did] =  diag(Jac)[did] + 2*betas[seq_len(v) + p*v]
   return(-Jac)
 }
--- R/F_respFunScoreMat.R
+++ R/F_respFunScoreMat.R
@@ -1,19 +1,22 @@
 #' Returns the derivative of the Lagrangian of the parameters of the parametric response function (a polynomial of any degree)
 #'
-#' @param betas: a (deg+1)xp matrix of regression parameters with deg the degree of the response function
+#' @param betas: a vector of length (deg+1)*(p+1) with regression parameters with deg the degree of the response function and the lagrangian multipliers
 #' @param aX: the nxp data matrix
-#' @param reg: a matrix of regressors with the dimension (deg+1)xn (offset + \mathbf{C} \boldsymbol{\alpha})
+#' @param reg: a matrix of regressors with the dimension nx(deg+1) (offset + \mathbf{C} \boldsymbol{\alpha})
 #' @param thetaMat: The n-by-p matrix with dispersion parameters
 #' @param mumarg: offset matrix of size nxp
-#' @param lambda2: A vector of length (deg+1) with the Lagrange multipliers
+#' @param psi a scalar, the importance parameter
+#' @param v an integer, one plus the degree of the response function
+#' @param p an integer, the number of taxa
 #'
 #' The parameters are restricted to be normalized, i.e. all squared intercepts, first order and second order parameters sum to 1
 #'
 #' @return The evaluation of the score functions, a vector of length (p+1)*(deg+1)
 #'
-respFunScoreMat = function(betas, aX, reg, thetaMat, muMarg, psi, lambda2) {
-  mu = exp(crossprod(reg, betas)*psi) * muMarg
-  score =  reg %*% ((aX-mu)/(1+mu/thetaMat)) *2*lambda2 *betas
-  norm = rowSums(betas^2) - 1
+respFunScoreMat = function(betas, aX, reg, thetaMat, muMarg, psi, p, v) {
+  NBparams = matrix(betas[seq_len(p*v)], ncol = p)
+  mu = exp(reg %*% NBparams*psi) * muMarg
+  score =  crossprod(reg*psi, (aX-mu)/(1+mu/thetaMat)) +2*betas[seq_len(v) + p*v] *NBparams
+  norm = rowSums(NBparams^2) - 1
   return(c(score, norm)) #Taxon per taxon
 }
