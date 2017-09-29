37d1380c2ffdcee88b2ccd26b9bbae95b2d50aa3
shawinke <stijn.hawinkel@ugent.be>
2017-09-29 10:05:43 +0200

    A note on the ordering of importance parameters

--- R/F_NBjacobianCol.R
+++ R/F_NBjacobianCol.R
@@ -13,11 +13,11 @@
 #' @param cMatK the lower dimensions of the colScores
 
 #' @return A matrix of dimension p+1+1+(k-1) with evaluations of the Jacobian
-NBjacobianCol = function(beta, X, reg, thetas, muMarg, k, n ,p, colWeights, nLambda, cMatK, constrained = FALSE){
+NBjacobianCol = function(beta, X, reg, thetas, muMarg, k, n ,p, colWeights, nLambda, cMatK){
   cMat = matrix(beta[1:p], byrow=TRUE, nrow=1, ncol=p)
 
   #Calculate the mean
-  mu = if (constrained){exp(rowMultiply(reg, c(cMat))) * muMarg} else {exp(reg %*% cMat) * muMarg}
+  mu = exp(reg %*% cMat) * muMarg
 
   Jac = matrix(0, nrow= p + nLambda, ncol=p + nLambda)
   #The symmetric jacobian matrix. The upper part is filled first, then mirror image is taken for lower triangle
@@ -37,10 +37,6 @@ NBjacobianCol = function(beta, X, reg, thetas, muMarg, k, n ,p, colWeights, nLam
   #Symmetrize
   Jac = Jac + t(Jac)
   #dLag²/dr_{ik}²
-  diag(Jac)[1:p] = if(constrained) {
-    -colSums(tmp * reg^2) + 2*beta[p+2]*colWeights
-  } else {
-    -crossprod(tmp, reg^2) + 2*beta[p+2]*colWeights
-  }
+  diag(Jac)[1:p] = -crossprod(tmp, reg^2) + 2*beta[p+2]*colWeights
   Jac
 }
