5470d2e257e0304bd5f486b349a6f980232ef9b8
shawinke <stijn.hawinkel@ugent.be>
2017-10-27 16:22:39 +0200

    Residual plots with runs test statistic

--- R/F_residualPlot.R
+++ R/F_residualPlot.R
@@ -7,8 +7,9 @@
 #' @param numTaxa an integer, the number of taxa to plot
 #' @param mfrow passed on to par(). If not supplied will be calculated based on numTaxa
 #'
-#'If whichTaxa is "run" or "response" the taxa with the highest run statistics or responses are plotted, numTax indicates the number. If whichTaxa is a character vector, these are interpreted as taxon names to plot. This function is mainly meant for linear response functions, but can be used for others too.
+#'If whichTaxa is "run" or "response" the taxa with the highest run statistics or responses are plotted, numTax indicates the number. If whichTaxa is a character vector, these are interpreted as taxon names to plot. This function is mainly meant for linear response functions, but can be used for others too. The runs test statistic from the tseries package is used.
 residualPlot = function(RCM, Dim = 1, whichTaxa = "response", resid = "Deviance", numTaxa = 9, mfrow = NULL){
+  require(tseries)
   sampleScore = RCM$covariates %*% RCM$alpha[,Dim, drop = FALSE]
   if(resid == "Deviance"){
     resMat = getDevianceRes(RCM, seq_len(Dim))
@@ -17,11 +18,11 @@ residualPlot = function(RCM, Dim = 1, whichTaxa = "response", resid = "Deviance"
       thetaMat = extractDisp(RCM, mu)
     resMat = (RCM$X-mu)/sqrt(mu+mu^2/thetaMat)
       } else {stop("Unknown residual type!")}
-if(!whichTaxa %in% c("run","response")){
+if(!whichTaxa %in% c("runs","response")){
   numTaxa = length(whichTaxa)
   idTaxa = whichTaxa
 } else {
-  sizes = if(whichTaxa =="run") getRunStats(resMat, sampleScore) else RCM$NB_params[2,,Dim] #Select taxa with longest runs or strongest responses
+  sizes = if(whichTaxa =="runs") apply(resMat>0,2, function(x){tseries::runs.test(factor(x))$statistic}) else RCM$NB_params[2,,Dim] #Select taxa with longest runs or strongest responses
   idTaxa = which(sizes >= sort(sizes, decreasing = TRUE)[numTaxa])
 }
   #Prepare the plotting facets
