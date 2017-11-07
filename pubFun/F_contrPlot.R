63b6f217ea78b989767e38e1f13eb55719188924
shawinke <stijn.hawinkel@ugent.be>
2017-11-07 17:23:50 +0100

    Corrected addOrthogonalProjection() function

--- pubFun/F_contrPlot.R
+++ pubFun/F_contrPlot.R
@@ -5,17 +5,18 @@
 #' @param expFac see addLegend
 #' @param groupMeth a factor specifying the order of the methods
 #' @param las,ylim,... passed on to the boxplot function
+#' @param diamCol The color of the diamond for the mean
 #'
 #' @return invisible(), plots the boxplot
-contrPlot = function(contr, method = NULL, las = 2, expFac = 1.2, groupMeth = droplevels(groupsMeth[names(groupsMeth) %in% names(contr)]), ylim = c(0,max(sapply(contr, quantile, 0.95, na.rm = TRUE))),...) {
+contrPlot = function(contr, method = NULL, las = 2, expFac = 1.2, groupMeth = droplevels(groupsMeth[names(groupsMeth) %in% levels(factorMeth)[levelsMeth %in% names(contr)] & if(is.null(method)) TRUE else names(groupsMeth) %in% method]), ylim = c(0,max(sapply(contr, quantile, 0.95, na.rm = TRUE))), diamCol = "orange",...) {
   moltenContr = orderDF(melt(contr, variable.name = "Method", value.name = "contrRatio", id.vars = NULL))
   if(!is.null(method)){
-    moltenContr = subset(moltenContr, moltenContr$Method %in% method)
+    moltenContr = droplevels(subset(moltenContr, moltenContr$Method %in% method))
   }
   meansDist = tapply(moltenContr$contrRatio, moltenContr$Method, mean, na.rm = TRUE)
   at = seq_along(meansDist)*expFac
-  boxplot(contrRatio~Method, main = "Mean contribution of DA taxa \n relative to the 50% most contributing non-DA taxa by method", data = moltenContr, ylab = "Mean contribution ratio", col = groupMeth, las = las, at = at, ylim = ylim,...)
-  points(y = meansDist, x = at, col="red", pch=18, cex = 1.6)
+  boxplot(contrRatio~Method, main = "Mean contribution of DA taxa relative to \n the 50% most contributing non-DA taxa by method", data = moltenContr, ylab = "Mean contribution ratio", col = groupMeth, las = las, at = at, ylim = ylim,...)
+  points(y = meansDist, x = at, col = diamCol, pch=18, cex = 1.6)
   abline(h=1, lty = "dashed")
-  addLegend(groupMeth = groupMeth, expFac = expFac)
+  addLegend(groupMeth = groupMeth, expFac = expFac, yloc = 5)
 }
