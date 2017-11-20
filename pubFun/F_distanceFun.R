673353794dbbe0918938f37b2fa734b974b7cccc
shawinke <stijn.hawinkel@ugent.be>
2017-11-20 10:14:09 +0100

    Switched to geometric means between the ratios

--- pubFun/F_distanceFun.R
+++ pubFun/F_distanceFun.R
@@ -1,4 +1,4 @@
-#' A function to calculate the within cluster and overall distances and the ratio between them
+#' A function to calculate the within cluster and overall distances and the geometric mean of the ratio between them
 #'
 #' @param coord an nxk matrix of coordinates
 #' @param clusters a factor of length n with cluster memberships
@@ -7,7 +7,7 @@
 #' @return a list with
 #' \item{withinDist} {within distances}
 #' \item{overalDist} {the overal distances}
-#' \item{ratio}{The mean ratio between the two}
+#' \item{ratio}{The geometric mean of the ratio between the two}
 distanceFun = function(coord, clusters, Dim = 1:3){
 
   coord = coord[,Dim, drop=FALSE]
@@ -26,6 +26,6 @@ distanceFun = function(coord, clusters, Dim = 1:3){
     dist(rbind(y, overalCenter))
   })
 
-  ratio = mean(unlist(sapply(unique(clusters), function(x){overalDist[clusters==x]/withinDist[[x]]})))
+  ratio = exp(mean(log(unlist(sapply(unique(clusters), function(x){overalDist[clusters==x]/withinDist[[x]]}))))) #The geometric mean
   list(withinDist = withinDist, overalDist =  overalDist, ratio = ratio)
 }
