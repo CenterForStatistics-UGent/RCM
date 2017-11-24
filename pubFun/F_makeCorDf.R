c67acc9c59d7bb917f6cef363a1cbfb1e2673fd0
shawinke <stijn.hawinkel@ugent.be>
2017-11-24 14:46:42 +0100

    Getting makeCorDf() to work with dispersions as well

--- pubFun/F_makeCorDf.R
+++ pubFun/F_makeCorDf.R
@@ -1,22 +1,18 @@
 #' A function that makes the correlation dataframe for plotting
 #'
 #' @param scores the list of scores
-#' @param datList the list of data matrices, or a list of lists containing the data matrix
+#' @param datList a list of lists containing the data matrix
 #' @param Dims which dimensions to consider
-#' @param scoreDim a character vector, "rows" or "columns": which margins to use to calculate the correlations
-#' @param dataMat a boolean, is datList a list of data matrices? Otherwise it is a list of lists
+#' @param scoreDim a character vector, "rows", "columns" or "dispersions": which margins to use to calculate the correlations, or the dispersions
 #' @param groupMeth a factor defining groups of the methods provided, based on their ordination paradigms
-#' @param dispersion a boolean, have overdispersion parameters been provided?
+
 #'
 #'@return a list of dataframes with "Correl" and "Method" components
-makeCorDf = function(scores, datList, Dims = 1:3, scoreDim = "rows", dataMat = TRUE,
-                     groupMeth = droplevels(factor(c(as.character(groupsMeth[names(groupsMeth) %in% names(scores[[1]])]), "Control"), levels = c(levels(groupsMeth), "Control"))), dispersion = FALSE){
-  if(!dataMat){
-    datList = lapply(datList, function(x){x$dataMat})
-  }
+makeCorDf = function(scores, datList, Dims = 1:3, scoreDim = "rows", groupMeth = droplevels(factor(c(as.character(groupsMeth[names(groupsMeth) %in% names(scores[[1]])]), "Control"), levels = c(levels(groupsMeth), "Control")))){
+
   cor0 = lapply(Dims, function(Dim){
       mapply(scores, datList, FUN = function(x,z){
-        margins = switch(scoreDim, "rows" = rowSums(z), "columns" = colSums(z))
+        margins = switch(scoreDim, "rows" = rowSums(x$dataMat), "columns" = colSums(x$dataMat), "dispersions"=x$thetasSampled)
         x$Control = matrix(rnorm(length(margins)), length(margins), length(Dims))
         sapply(x , function(y){libCor(y, margins = margins, Dim = Dim)}) #Include a control
       }, SIMPLIFY = TRUE)
