443db802642fdebfed8672d64cbb5634b70883db
shawinke <stijn.hawinkel@ugent.be>
2018-11-06 15:24:48 +0100

    vgam.edit runs

--- R/F_getLogLik.R
+++ R/F_getLogLik.R
@@ -6,7 +6,7 @@
 #' @return A matrix with logged likelihood of the size of the data matrix
 getLogLik = function(rcm, Dim){
   E = extractE(rcm, Dim)
-  if(Dim[1] == Inf) dpois(x = rcm$X, lambda = rcm$X, log = TRUE)
+  if(Dim[1] == Inf) {return(dpois(x = rcm$X, lambda = rcm$X, log = TRUE))}
   thetaMat = extractDisp(rcm, E)
   dnbinom(x = rcm$X, mu = E, size = thetaMat, log = TRUE)
 }
