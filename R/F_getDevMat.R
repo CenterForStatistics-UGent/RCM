58dc2e91d46c86196ebf8d781f16fe1735eecc93
shawinke <stijn.hawinkel@ugent.be>
2018-11-07 12:32:53 +0100

    Correct for numerical NA's

--- R/F_getDevMat.R
+++ R/F_getDevMat.R
@@ -6,7 +6,8 @@
 #'
 #'@return The matrix of deviance residuals
 getDevMat = function(X, thetaMat, mu){
-  tmpMat = suppressWarnings(sqrt(2*(X*log(X/mu)-(X+thetaMat)*log((1+X/thetaMat)/(1+mu/thetaMat))))*sign(X-mu))
+  tmpMat = sqrt(2*(X*log(X/mu)-(X+thetaMat)*log((1+X/thetaMat)/(1+mu/thetaMat))))*sign(X-mu)
   tmpMat[X==0] = -sqrt((2*thetaMat*log(1+mu/thetaMat))[X==0]) #zero observations are always smaller than the mean
+  tmpMat[is.na(tmpMat)] = 0 #Correct for numerical NAs
   tmpMat
 }
