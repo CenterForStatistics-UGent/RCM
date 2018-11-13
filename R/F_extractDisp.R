de4a78f7aa25a7f1ac38fc4f84a19faad79b0377
shawinke <stijn.hawinkel@ugent.be>
2018-11-13 16:42:04 +0100

    No longer need dispersion extraction

--- R/F_extractDisp.R
+++
@@ -1,9 +0,0 @@
-#' A function that returns dispersions belonging to a RCM object
-#'
-#' @param rcm an RCM object
-#' @param E a matrix of expectations
-#'
-#' @return a matrix of dispersions
-extractDisp = function(rcm, E){
-    matrix(estDisp(X = rcm$X, muMarg = E), byrow = TRUE, nrow = nrow(rcm$X), ncol = ncol(rcm$X))
-}
