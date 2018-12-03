17975c85e77c3173ab39ddd01d4e689e6ae9f15d
Joris Meys <Joris.Meys@UGent.be>
2018-12-03 18:12:31 +0100

    Add S4 logic for RCM

--- R/F_RCM.R
+++ R/F_RCM.R
@@ -16,99 +16,126 @@
 #'@seealso \code{\link{RCM_NB}},\code{\link{plot.RCM}},\code{\link{residualPlot}},\code{\link{plotRespFun}}
 #'
 #' @return see \code{\link{RCM_NB}}
-#' @importFrom nleqslv nleqslv
-#' @importFrom tensor tensor
-#' @importFrom alabama constrOptim.nl
-#' @import VGAM
-#' @import phyloseq
-#' @importFrom stats as.formula contrasts dnbinom dpois glm integrate quantile
-#' @export
+#' 
 #' @examples
 #' data(Zeller)
 #' require(phyloseq)
 #' tmpPhy = prune_taxa(taxa_names(Zeller)[1:100],
 #' prune_samples(sample_names(Zeller)[1:50], Zeller))
 #' zellerRCM = RCM(tmpPhy, round = TRUE)
-RCM = function(dat, k = 2, round=FALSE, prevCutOff = 0.05, minFraction = 0.1, rowWeights = "uniform", colWeights = "marginal", covariates = NULL, confounders = NULL, ...){
-  classDat = class(dat) #The class provided
+#' 
+#' @importFrom nleqslv nleqslv
+#' @importFrom tensor tensor
+#' @importFrom alabama constrOptim.nl
+#' @import VGAM
+#' @import phyloseq
+#' @importFrom stats as.formula contrasts dnbinom dpois glm integrate quantile
+#' 
+#' @rdname RCM
+#' @export
+setGeneric("RCM", 
+           function(dat, ...) standardGeneric("RCM"))
 
-  ##The count data##
-  if (classDat=="matrix"){
-    X = dat
-  }else  if(classDat=="phyloseq"){
-    X = if (taxa_are_rows(dat)) t(otu_table(dat)@.Data) else otu_table(dat)@.Data
-  } else {stop("Please provide a matrix or a phyloseq object! \n")}
-  p=ncol(X); n=nrow(X)
+#' @rdname RCM
+#' @export
+setMethod("RCM",
+          "phyloseq",
+          function(dat, ...){
+            # Safer than using @.Data, as this uses the standard conversion
+            # function of the package as opposed to the internal representation.
+            dat <- as(otu_table(dat), "matrix")
+            if (taxa_are_rows(dat)) 
+              dat <- t(dat)
+            
+            # Call generic again to pass on data.
+            RCM(dat, ...)
+          })
 
-  if(is.null(colnames(X))){colnames(X)=seq_len(ncol(X))}
-  if(is.null(rownames(X))){rownames(X)=seq_len(nrow(X))}
-  colNames = colnames(X); rowNames =rownames(X)
-  if(round) {X=round(X, 0) }#Round to integer
-
-  #Check X type
-  if(!all(floor(X)==X)){stop("Please provide integer count matrix (not a matrix of proportions!), or set 'round' to TRUE! \n")
-  } else{X=matrix(as.integer(X), ncol=p, nrow=n)}
-
-  colnames(X)=colNames; rownames(X)=rowNames
-  X=X[, (colMeans(X==0)<(1-prevCutOff)) & (colSums(X)>(n*minFraction))] #Remove txa with low prevalence and those who do not meet the minFraction requirement
-  if(any(rowSums(X)==0)){warning(immediate. = TRUE, paste0("Samples \n", paste(collapse = ", ", rownames(X)[rowSums(X)==0]), "\n contained no more reads after trimming taxa with low prevalence, and were excluded from the fit"))}
-  rowIDkeep = rowSums(X)>0
-  X=X[rowIDkeep, ] #Remove empty samples
-  n=nrow(X)
-  if(classDat=="phyloseq"){
-    dat = prune_samples(x = dat, samples = rowIDkeep)
-  } else {
-  confounders = confounders[rowIDkeep, ]
-  covariates = covariates[rowIDkeep, ]
-  }
-
-  ##Build confounder matrix if applicable. ##
-  if(!is.null(confounders)){
-tmp = buildConfMat(confounders, n, dat)
-confModelMatTrim = tmp$confModelMatTrim
-confModelMat = tmp$confModelMat
-rm(tmp)
-  } else{
-    confModelMat = confModelMatTrim = NULL
-  }
-
-  ##Build covariate matrix if applicable##
-  if(!is.null(covariates)){
-    tmp = buildCovMat(covariates, n, dat)
-    covModelMat = tmp$covModelMat
-    covariates = tmp$covariates
-    #Already prepare the matrix that defines the equations for centering the coefficients of the dummy variables
-    tmp = buildCentMat(tmp$datFrame)
-    centMat = tmp$centMat
-    datFrame = tmp$datFrame
-    rm(tmp)
-
-  #Remove rows with NA's, we might want to find something better or leave it to the end user
-  if(anyNA(datFrame)){
-    NArows = apply(datFrame, 1, anyNA)
-    if(all(NArows)){ stop("All samples have missing covariates")}
-    X = X[!NArows,]
-    datFrame = datFrame[!NArows,]
-    if(!is.null(confModelMat)){
-      confModelMat = confModelMat[!NArows,]
-      confModelMatTrim = confModelMatTrim[!NArows,]
-    }
-    warning(paste("Some covariates contain missing values. We removed samples \n", paste(which(NArows), collapse = ", "), "\n prior to analysis." ),immediate. = TRUE)
-    }
-  } else {
-    covModelMat = centMat = NULL
-  }
-if(anyNA(X)) {stop("NA values present in count matrix, please filter these out first!\n")}
-  tic = proc.time() #Time the calculation
-  tmp = RCM_NB(X, rowWeights = rowWeights, colWeights = colWeights, k = k,
+#' @rdname RCM
+#' @export
+setMethod("RCM",
+          "matrix",
+          function(dat, k = 2, round=FALSE, 
+                   prevCutOff = 0.05, minFraction = 0.1, 
+                   rowWeights = "uniform", colWeights = "marginal", 
+                   covariates = NULL, confounders = NULL, ...){
+            X <- dat
+            
+            p=ncol(X); n=nrow(X)
+            
+            if(is.null(colnames(X))){colnames(X)=seq_len(ncol(X))}
+            if(is.null(rownames(X))){rownames(X)=seq_len(nrow(X))}
+            colNames = colnames(X); rowNames =rownames(X)
+            if(round) {X=round(X, 0) }#Round to integer
+            
+            #Check X type
+            if(!all(floor(X)==X)){
+              stop("Please provide integer count matrix (not a matrix of proportions!), or set 'round' to TRUE! \n")
+            } else{
+              X=matrix(as.integer(X), ncol=p, nrow=n)
+              }
+            
+            colnames(X)=colNames; rownames(X)=rowNames
+            X=X[, (colMeans(X==0)<(1-prevCutOff)) & (colSums(X)>(n*minFraction))] #Remove txa with low prevalence and those who do not meet the minFraction requirement
+            if(any(rowSums(X)==0)){warning(immediate. = TRUE, paste0("Samples \n", paste(collapse = ", ", rownames(X)[rowSums(X)==0]), "\n contained no more reads after trimming taxa with low prevalence, and were excluded from the fit"))}
+            rowIDkeep = rowSums(X)>0
+            X=X[rowIDkeep, ] #Remove empty samples
+            n=nrow(X)
+            if(classDat=="phyloseq"){
+              dat = prune_samples(x = dat, samples = rowIDkeep)
+            } else {
+              confounders = confounders[rowIDkeep, ]
+              covariates = covariates[rowIDkeep, ]
+            }
+            
+            ##Build confounder matrix if applicable. ##
+            if(!is.null(confounders)){
+              tmp = buildConfMat(confounders, n, dat)
+              confModelMatTrim = tmp$confModelMatTrim
+              confModelMat = tmp$confModelMat
+              rm(tmp)
+            } else{
+              confModelMat = confModelMatTrim = NULL
+            }
+            
+            ##Build covariate matrix if applicable##
+            if(!is.null(covariates)){
+              tmp = buildCovMat(covariates, n, dat)
+              covModelMat = tmp$covModelMat
+              covariates = tmp$covariates
+              #Already prepare the matrix that defines the equations for centering the coefficients of the dummy variables
+              tmp = buildCentMat(tmp$datFrame)
+              centMat = tmp$centMat
+              datFrame = tmp$datFrame
+              rm(tmp)
+              
+              #Remove rows with NA's, we might want to find something better or leave it to the end user
+              if(anyNA(datFrame)){
+                NArows = apply(datFrame, 1, anyNA)
+                if(all(NArows)){ stop("All samples have missing covariates")}
+                X = X[!NArows,]
+                datFrame = datFrame[!NArows,]
+                if(!is.null(confModelMat)){
+                  confModelMat = confModelMat[!NArows,]
+                  confModelMatTrim = confModelMatTrim[!NArows,]
+                }
+                warning(paste("Some covariates contain missing values. We removed samples \n", paste(which(NArows), collapse = ", "), "\n prior to analysis." ),immediate. = TRUE)
+              }
+            } else {
+              covModelMat = centMat = NULL
+            }
+            if(anyNA(X)) {stop("NA values present in count matrix, please filter these out first!\n")}
+            tic = proc.time() #Time the calculation
+            tmp = RCM_NB(X, rowWeights = rowWeights, colWeights = colWeights, k = k,
                          confounders  = list(confounders = confModelMat, confoundersTrim = confModelMatTrim),
                          covariates = covModelMat, prevCutOff = prevCutOff, minFraction = minFraction, centMat = centMat,...)
-  if(classDat=="phyloseq"){tmp$physeq = prune_samples(rownames(tmp$X),prune_taxa(colnames(tmp$X),dat)) }
-  tmp = within(tmp, {
-    runtimeInMins = (proc.time()-tic)[1]/60 # Sum the runtimes
-    k = k #Store number of dimensions
-  })
-  tmp$call = match.call()
-  class(tmp) = "RCM"
-  tmp
-}
+            if(classDat=="phyloseq"){tmp$physeq = prune_samples(rownames(tmp$X),prune_taxa(colnames(tmp$X),dat)) }
+            tmp = within(tmp, {
+              runtimeInMins = (proc.time()-tic)[1]/60 # Sum the runtimes
+              k = k #Store number of dimensions
+            })
+            tmp$call = match.call()
+            class(tmp) = "RCM"
+            tmp
+          })
+
